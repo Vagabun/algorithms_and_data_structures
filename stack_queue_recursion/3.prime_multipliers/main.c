df750bb9a3b4081f39ba1de5aa8899e7f514b223
Vagabun <16858610+Vagabun@users.noreply.github.com>
2017-08-21 20:34:57 +0200

    3.prime_multipliers: finish task, sent for test

--- stack_queue_recursion/3.prime_multipliers/main.c
+++ stack_queue_recursion/3.prime_multipliers/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define MAX_SIZE 10010
+//#define MIN(a, b) ((a < b) ? a : b)
 
 struct q {
     unsigned long long int q[MAX_SIZE];
@@ -31,13 +32,49 @@ void dequeue(QUEUE *inst) {
     inst->top += 1;
 }
 
+unsigned long long int min(unsigned long long int a, unsigned long long int b) {
+    unsigned long long int m;
+    if (a < b) m = a;
+    else m = b;
+    return m;
+}
+
 int main() {
+
+    FILE *input = fopen("input.txt", "r");
+    FILE *output = fopen("output.txt", "w");
+
+    int N;
+    long int P1, P2, P3;
+    fscanf(input, "%d %li %li %li", &N, &P1, &P2, &P3);
 
     QUEUE Q1, Q2, Q3;
 
     make(&Q1);
+    make(&Q2);
+    make(&Q3);
 
-    printf("%d", Q1.top);
+    enqueue(P1, &Q1);
+    enqueue(P2, &Q2);
+    enqueue(P3, &Q3);
+
+    int n = 1;
+    unsigned long long int m;
+    while (n < N) {
+        m = min(min(top(&Q1), top(&Q2)), top(&Q3));
+        n++;
+        enqueue(m*P1, &Q1);
+        enqueue(m*P2, &Q2);
+        enqueue(m*P3, &Q3);
+        if (top(&Q1) == m) dequeue(&Q1);
+        if (top(&Q2) == m) dequeue(&Q2);
+        if (top(&Q3) == m) dequeue(&Q3);
+    }
+
+    fprintf(output, "%llu", m);
+
+    fclose(input);
+    fclose(output);
 
     return 0;
 }
