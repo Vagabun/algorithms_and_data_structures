016010fea15d09ca8c043adff464ad69deeccc6f
Vagabun <16858610+Vagabun@users.noreply.github.com>
2017-08-30 18:43:01 +0200

    5.partial sums 1 - initial, create example of input, sent for test

---
+++ stack_queue_recursion/5.partial_sums_1/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAXSIZE 1000010
+
+void precalc(long int *part_sums, const long int *arr, const long int n) {
+    int i;
+    part_sums[0] = 0;
+    for (i = 1; i <= n; ++i)
+        part_sums[i] = part_sums[i-1] + arr[i];
+}
+
+int main() {
+
+    FILE *input = fopen("input.txt", "r");
+    FILE *output = fopen("output.txt", "w");
+
+    long int n, Q, a, b;
+    int i;
+
+    long int *arr = malloc(MAXSIZE * sizeof(long int));
+    long int *part_sums = malloc(MAXSIZE * sizeof(long int));
+
+    fscanf(input, "%li %li", &n, &Q);
+    for (i = 1; i <= n; ++i) {
+        fscanf(input, "%li", &arr[i]);
+    }
+
+    precalc(part_sums, arr, n);
+
+    for (int j = 0; j < Q; ++j) {
+        fscanf(input, "%li %li", &a, &b);
+        fprintf(output, "%li ", part_sums[b]-part_sums[a-1]);
+    }
+
+    return 0;
+}
