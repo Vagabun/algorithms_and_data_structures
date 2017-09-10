192ea29a92a30b2c536db32e7093b355563f4d4e
Vagabun <16858610+Vagabun@users.noreply.github.com>
2017-09-11 00:07:17 +0200

    add test generator, sorting and search: 2.quick sorting - add simple test for merge sort

---
+++ test_generator/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+
+    FILE *output = fopen("input.txt", "w");
+    unsigned long x, i;
+    srand(time(NULL));
+    for (i = 0; i < 1000000; ++i) {
+        x = rand();
+        x <<= 15;
+        x ^= rand();
+        x %= 1000001;
+        fprintf(output, "%li ", x);
+    }
+
+//    while (fscanf(output, "%li", &i) != EOF) {
+//        if (i == 5115) printf("%li\n", i);
+//    }
+    fclose(output);
+    return 0;
+}
