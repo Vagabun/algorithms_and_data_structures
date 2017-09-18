5531deea83b6fb8103c7a631c5f58f5562767c5f
Vagabun <16858610+Vagabun@users.noreply.github.com>
2017-09-18 17:59:57 +0200

    sorting and search: 2.quick sorting: heapsort - rewrite with using static array, sent for test

--- sorting_and_search/2.quick_sorting/heapsort/main.c
+++ sorting_and_search/2.quick_sorting/heapsort/main.c
@@ -1,47 +1,67 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-struct h {
-    long long l;
-    long long *h = malloc(3000010 * sizeof(long long));
-};
+long long heap[3000010];
 
-typedef struct h HEAP;
-
-void make(HEAP *instance) {
-    instance->l = 0;
-}
-
-void lift(HEAP *instance, long long c) {
-    while (c > 1 && instance->h[c] > instance->h[c/2]) {
+void lift(long long c) {
+    //c - position
+    while (c > 1 && heap[c] > heap[c/2]) {
         long long p = c/2;
-        long long tmp = instance->h[c];
-        instance->h[c] = instance->h[p];
-        instance->h[p] = tmp;
+        long long tmp = heap[c];
+        heap[c] = heap[p];
+        heap[p] = tmp;
         c = p;
     }
 }
 
-void put(HEAP *instance, long long n) {
-    ++instance->l;
-    instance->h[instance->l] = n;
-    lift(instance, instance->l);
+void shift(long long p, const long long l) {
+    while (p * 2 <= l) {
+        long long c = p * 2;
+        if (c < l && heap[c+1] > heap[c])
+            ++c;
+        if (heap[p] >= heap[c])
+            break;
+        long long tmp = heap[p];
+        heap[p] = heap[c];
+        heap[c] = tmp;
+        p = c;
+    }
 }
 
+void get(long long *l) {
+    long long tmp = heap[1];
+    heap[1] = heap[*l];
+    heap[*l] = tmp;
+    --*l;
+    shift(1, *l);
+}
 
+void put(long long n, long long *l) {
+    ++*l;
+    heap[*l] = n;
+    lift(*l);
+}
+
+void heapsort(long long *l) {
+    long long i;
+    for (i = *l; i >= 1; --i)
+        get(l);
+}
 
 int main() {
 
     FILE *input = fopen("input.txt", "r");
     FILE *output = fopen("output.txt", "w");
 
-    HEAP H;
-    make(&H);
-
-    long long n, counter = 1;
+    long long n, counter, l = 0;
     while (fscanf(input, "%lli", &n) != EOF) {
-        put(&H, n);
-        ++counter;
+        put(n, &l);
+    }
+    counter = l;
+
+    heapsort(&l);
+
+    for (int i = 1; i <= counter; ++i) {
+        fprintf(output, "%lli ", heap[i]);
     }
 
     return 0;
