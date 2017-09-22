8c17f4e6635edd3ac84ee3bbb0fae4aeb3c94d68
Vagabun <16858610+Vagabun@users.noreply.github.com>
2017-09-22 20:16:16 +0200

    sorting and search: 3.O(n): count sort - dumb way

--- sorting_and_search/3.O(n)/count_sort/main.c
+++ sorting_and_search/3.O(n)/count_sort/main.c
@@ -1,18 +1,26 @@
 #include <stdio.h>
 #include <string.h>
 
-long long arr[3000010];
+long long arr[3000010] = { 0 };
 
 int main() {
 
     FILE *input = fopen("input.txt", "r");
     FILE *output = fopen("output.txt", "w");
 
-    memset(arr, 0, 3000010 * sizeof(arr));
+//    memset(arr, 0, 3000010);
     long long n, counter = 0;
-//    while (fscanf(input, "%lli", &n) != EOF) {
-//
-//    }
-//    printf("%lli", arr[3000001]);
+    while (fscanf(input, "%lli", &n) != EOF) {
+        ++arr[n];
+        ++counter;
+    }
+    long long i, j;
+    for (i = 0; i <= counter; ++i) {
+        if (arr[i] > 0) {
+            for (j = 0; j < arr[i]; ++j)
+                fprintf(output, "%lli ", i);
+        }
+    }
+
     return 0;
 }
