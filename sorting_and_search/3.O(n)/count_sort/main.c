554df198999a7ff6b64b556cbd0b0a9703da25fb
Vagabun <16858610+Vagabun@users.noreply.github.com>
2017-09-22 15:27:48 +0200

    sorting and search: 3.O(n): count sort - intial

---
+++ sorting_and_search/3.O(n)/count_sort/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+long long arr[3000010];
+
+int main() {
+
+    FILE *input = fopen("input.txt", "r");
+    FILE *output = fopen("output.txt", "w");
+
+    memset(arr, 0, 3000010 * sizeof(arr));
+    long long n, counter = 0;
+//    while (fscanf(input, "%lli", &n) != EOF) {
+//
+//    }
+//    printf("%lli", arr[3000001]);
+    return 0;
+}
