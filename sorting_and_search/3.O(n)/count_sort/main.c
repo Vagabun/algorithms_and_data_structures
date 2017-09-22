8778ac6b6c4e37f705723e8dd6d0a55e93e4244f
Vagabun <16858610+Vagabun@users.noreply.github.com>
2017-09-22 21:49:37 +0200

    sorting and search: 3.O(n): count sort - rewrite (different algorithm), sent for test

--- sorting_and_search/3.O(n)/count_sort/main.c
+++ sorting_and_search/3.O(n)/count_sort/main.c
@@ -1,26 +1,42 @@
 #include <stdio.h>
-#include <string.h>
 
-long long arr[3000010] = { 0 };
+long long x[3000010];
+long long z[3000010] = { 0 };
+long long y[3000010];
 
 int main() {
 
     FILE *input = fopen("input.txt", "r");
     FILE *output = fopen("output.txt", "w");
 
-//    memset(arr, 0, 3000010);
-    long long n, counter = 0;
+    long long n, counter = 1;
     while (fscanf(input, "%lli", &n) != EOF) {
-        ++arr[n];
+        x[counter] = n;
         ++counter;
     }
-    long long i, j;
-    for (i = 0; i <= counter; ++i) {
-        if (arr[i] > 0) {
-            for (j = 0; j < arr[i]; ++j)
-                fprintf(output, "%lli ", i);
-        }
+
+    long long min, max, i;
+    min = max = x[1];
+    for (i = 2; i < counter; ++i) {
+        if (x[i] > max)
+            max = x[i];
+        else if (x[i] < min)
+            min = x[i];
     }
+
+    for (i = 1; i < counter; ++i)
+        ++z[x[i]];
+
+    for (i = min + 1; i <= max; ++i)
+        z[i] += z[i-1];
+
+    for (i = 1; i < counter; ++i) {
+        y[z[x[i]]] = x[i];
+        --z[x[i]];
+    }
+
+    for (i = 1; i < counter; ++i)
+        fprintf(output, "%lli ", y[i]);
 
     return 0;
 }
