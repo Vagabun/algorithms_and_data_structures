17a7e2ec77947109d62950b1194dcece28e60484
Vagabun <16858610+Vagabun@users.noreply.github.com>
2017-10-01 16:54:05 +0200

    4.median of medians - finished, sent for test

--- sorting_and_search/4.median_of_medians/main.c
+++ sorting_and_search/4.median_of_medians/main.c
@@ -12,35 +12,6 @@ long long random(long long n) {
     return x;
 }
 
-void quicksort(long long a, long long b) {
-    if (a >= b)
-        return;
-    //pivot point
-    long long m = random(b - a + 1) + a;
-    long long k = arr[m];
-    long long l = a - 1;
-    long long r = b + 1;
-    while (1) {
-        while (1) {
-            ++l;
-            if (arr[l] >= k)
-                break;
-        }
-        while (1) {
-            --r;
-            if (arr[r] <= k)
-                break;
-        }
-        if (l >= r)
-            break;
-        long long tmp = arr[l];
-        arr[l] = arr[r];
-        arr[r] = tmp;
-    }
-    quicksort(a, r);
-    quicksort(r+1, b);
-}
-
 long long search(long long a, long long b, long long k) {
     if (a == b)
         return arr[a];
@@ -65,10 +36,12 @@ long long search(long long a, long long b, long long k) {
         arr[l] = arr[r];
         arr[r] = tmp;
     }
-    if (k <= r)
-        return search(a, r, k);
+    l = r;
+    r += 1;
+    if (k <= l)
+        return search(a, l, k);
     else
-        return search(r+1, b, k);
+        return search(r, b, k);
 }
 
 int main() {
@@ -85,14 +58,7 @@ int main() {
 
     srand(time(NULL));
 
-    printf("%lli", search(0, counter-1, k-1));
-
-
-//    for (int i = 0; i < counter; ++i)
-//        fprintf(output, "%lli ", arr[i]);
-
-//    fclose(input);
-//    fclose(output);
+    fprintf(output, "%lli", search(0, counter-1, k-1));
 
     return 0;
 }
