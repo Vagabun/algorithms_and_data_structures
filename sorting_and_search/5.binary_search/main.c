5da916d41f61c830e03674fbed11631a3b43357c
Vagabun <16858610+Vagabun@users.noreply.github.com>
2017-10-08 16:55:49 +0200

    5.binary search - in progress

--- sorting_and_search/5.binary_search/main.c
+++ sorting_and_search/5.binary_search/main.c
@@ -2,19 +2,88 @@
 
 long long x[1000010];
 
+long long binary_search_1(long long n, long long k) {
+    long long a = 1, b = n;
+    // check invariant
+    if (x[a] > k)
+        return 0;
+    if (x[a] == k)
+        return a;
+    if (x[b] < k)
+        return 0;
+    while (a + 1 < b) {
+        long long m = (a + b)/2;
+        if (x[m] < k)
+            a = m;
+        else
+            b = m;
+    }
+    if (x[b] == k)
+        return b;
+    else
+        return 0;
+}
+
+long long binary_search_2(long long n, long long k) {
+    long long a = 1, b = n;
+    if (x[a] > k)
+        return 0;
+    if (x[b] < k)
+        return b;
+    if (x[b] == k)
+        return b;
+    while (a + 1 < b) {
+        long long m = (a + b)/2;
+        if (x[m] <= k)
+            a = m;
+        else
+            b = m;
+    }
+    if (x[a] <= k)
+        return a;
+    else
+        return 0;
+}
+
+long long binary_search_3(long long n, long long k) {
+    long long a = 1, b = n;
+    if (x[a] < k)
+        return 0;
+    if (x[b] > k)
+        return b;
+    if (x[b] == k)
+        return b;
+    while (a + 1 < b) {
+        long long m = (a + b)/2;
+        if (x[m] >= k)
+            a = m;
+        else
+            b = m;
+    }
+    if (x[a] >= k)
+        return a;
+    else
+        return 0;
+}
+
+
 int main() {
 
     FILE *input = fopen("input.txt", "r");
     FILE *output = fopen("output.txt", "w");
 
     char c;
-    long long k, n, counter = 0;
+    long long k, n, counter = 1;
     fscanf(input, "%c %lli", &c, &k);
-//    while (fscanf(input, "%lli", &n) != EOF) {
-//        x[counter] = n;
-//        ++counter;
-//    }
-    fprintf(output, "%c %lli", c, k);
+    while (fscanf(input, "%lli", &n) != EOF) {
+        x[counter] = n;
+        ++counter;
+    }
+
+//    printf("%lli", counter);
+
+    printf("%lli", binary_search_3(counter - 1, k));
+//    fprintf(output, "%c %lli", c, k);
 
     fclose(input);
     fclose(output);
