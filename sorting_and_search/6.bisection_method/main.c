96c7b3e48ba440ea820691eec3a50b03cc1f3779
Vagabun <16858610+Vagabun@users.noreply.github.com>
2017-10-09 18:02:02 +0200

    6.bisection method - in progress

--- sorting_and_search/6.bisection_method/main.c
+++ sorting_and_search/6.bisection_method/main.c
@@ -5,11 +5,25 @@ double f(double a, double b, double c, double d, double x) {
     return a*log(b + x) - c/(d + x);
 }
 
+double bisection(double l, double r, double a, double b, double c, double d) {
+    double fA = f(a, b, c, d, l), m;
+    do {
+        m = (l + r)/2;
+        if (m == l || m == r)
+            return m;
+        if (fA * f(a, b, c, d, m) < 0)
+            r = m;
+        else
+            l = m;
+    } while ((l + r)/2 != l && (l + r)/2 != r);
+    printf("%lf", m);
+}
 int main() {
     FILE *input = fopen("input.txt", "r");
     FILE *output = fopen("output.txt", "w");
 
     double a, b, c, d;
     fscanf(input, "%lf %lf %lf %lf", &a, &b, &c, &d);
+    printf("%lf", bisection(0, 1e10, a, b, c, d));
     return 0;
 }
