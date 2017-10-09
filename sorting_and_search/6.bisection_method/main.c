4e59ce751d47897ff2c4e7a1edeb7013d43eb4bd
Vagabun <16858610+Vagabun@users.noreply.github.com>
2017-10-09 18:33:01 +0200

    6.bisection method - some corrections, sent for test

--- sorting_and_search/6.bisection_method/main.c
+++ sorting_and_search/6.bisection_method/main.c
@@ -1,29 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 
-double f(double a, double b, double c, double d, double x) {
-    return a*log(b + x) - c/(d + x);
+struct {
+    double a, b, c, d;
+} eq_data;
+
+double f(double x) {
+    return eq_data.a*log(eq_data.b + x) - eq_data.c/(eq_data.d + x);
 }
 
-double bisection(double l, double r, double a, double b, double c, double d) {
-    double fA = f(a, b, c, d, l), m;
+double bisection(double l, double r) {
+    double fA = f(l), m;
     do {
         m = (l + r)/2;
         if (m == l || m == r)
             return m;
-        if (fA * f(a, b, c, d, m) < 0)
+        if (fA * f(m) < 0)
             r = m;
         else
             l = m;
     } while ((l + r)/2 != l && (l + r)/2 != r);
-    printf("%lf", m);
+    return m;
 }
 int main() {
     FILE *input = fopen("input.txt", "r");
     FILE *output = fopen("output.txt", "w");
 
-    double a, b, c, d;
-    fscanf(input, "%lf %lf %lf %lf", &a, &b, &c, &d);
-    printf("%lf", bisection(0, 1e10, a, b, c, d));
+    fscanf(input, "%lf %lf %lf %lf", &eq_data.a, &eq_data.b, &eq_data.c, &eq_data.d);
+    fprintf(output, "%.6lf", bisection(0, 1e10));
     return 0;
 }
