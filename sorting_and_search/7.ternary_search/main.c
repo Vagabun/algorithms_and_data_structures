cb4fc49d2665bba07f24dd435e7552b80e927957
Vagabun <16858610+Vagabun@users.noreply.github.com>
2017-10-12 00:53:16 +0200

    7.ternary search - initial, sent for test

---
+++ sorting_and_search/7.ternary_search/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+long long arr[1000010];
+
+long long ternary_search(long long a, long long b) {
+    while (b > a + 2) {
+        long long h = (b - a)/3, l = a + h, r = b - h;
+        if (arr[l] < arr[r])
+            a = l;
+        else
+            b = r;
+    }
+    return arr[(a + b)/2];
+}
+
+int main() {
+
+    FILE *input = fopen("input.txt", "r");
+    FILE *output = fopen("output.txt", "w");
+
+    long long n, counter = 1;
+    while (fscanf(input, "%lli", &n) != EOF) {
+        arr[counter] = n;
+        ++counter;
+    }
+
+    fprintf(output, "%lli", ternary_search(0, counter));
+    return 0;
+}
