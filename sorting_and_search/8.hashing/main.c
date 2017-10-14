7225847b98dfd713805f028068c3212a8d221e6b
Vagabun <16858610+Vagabun@users.noreply.github.com>
2017-10-14 20:38:35 +0200

    8.hashing - in progress

--- sorting_and_search/8.hashing/main.c
+++ sorting_and_search/8.hashing/main.c
@@ -3,6 +3,8 @@
 #include <string.h>
 //#include <windows.h>
 
+char hash_table[10000][20];
+
 int main() {
 
     system("chcp 1251");
@@ -10,25 +12,37 @@ int main() {
     FILE *input = fopen("input.txt", "r");
     FILE *output = fopen("output.txt", "w");
 
-    char c, w;
-    char word[15];
+
+    char c, word[15];
     while (1) {
         fscanf(input, "%c", &c);
         if (c == '?' || c == '-' || c == '+') {
-            int i = 0;
-            while (1) {
-                fscanf(input, "%c", &w);
-                if (w == '\n')
-                    break;
-                word[i] = w;
-                ++i;
-            }
+            fscanf(input, "%s", word);
             printf("%s\n", word);
-            memset(word, 0, sizeof(word));
         }
         if (c == 'E')
             break;
     }
+
+//    char c, w;
+//    char word[15];
+//    while (1) {
+//        fscanf(input, "%c", &c);
+//        if (c == '?' || c == '-' || c == '+') {
+//            int i = 0;
+//            while (1) {
+//                fscanf(input, "%c", &w);
+//                if (w == '\n')
+//                    break;
+//                word[i] = w;
+//                ++i;
+//            }
+//            printf("%s\n", word);
+//            memset(word, 0, sizeof(word));
+//        }
+//        if (c == 'E')
+//            break;
+//    }
 
     fclose(input);
     fclose(output);
