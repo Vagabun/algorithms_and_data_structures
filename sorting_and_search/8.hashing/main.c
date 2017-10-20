a6c3d44f5bc18e1d89169865313e630eb807437d
Vagabun <16858610+Vagabun@users.noreply.github.com>
2017-10-20 23:55:46 +0200

    8.hashing - in progress of fixing incorrect work

--- sorting_and_search/8.hashing/main.c
+++ sorting_and_search/8.hashing/main.c
@@ -22,8 +22,14 @@ void compute_pow() {
 long long hash_f(const char* str) {
     long long hash = 0;
     int j;
-    for (j = 0; str[j] != '\0'; ++j)
-        hash += (str[j] - 'à' + 1) * p_pow[j];
+    for (j = 0; str[j] != '\0'; ++j) {
+        hash += abs(str[j] - '0') * p_pow[j];
+//        if (str[j] - '0' > 0)
+//            hash += (str[j] - 'a' + 1) * p_pow[j];
+//        else
+//            hash += (str[j] - 'à' + 1) * p_pow[j];
+//        hash += (str[j] - 'à' + 1) * p_pow[j];
+    }
     return hash % HASH_SIZE;
 }
 
@@ -36,29 +42,35 @@ void make_hash_table() {
 
 //search using linear probing
 long long search_f(const char* str) {
-    long long i = hash_f(str), j = 1;
+    long long i = hash_f(str); //j = 1;
     while (1) {
         if (strcmp(hash_table[i], "EMPTY") == 0)
             return -1;
         if (strcmp(hash_table[i], str) == 0)
             return i;
-        i = (i + j) % HASH_SIZE;
-        ++j;
+        i = (i + 1) % HASH_SIZE;
+//        ++j;
+//        ++i;
     }
 }
 
 //add value to hash table
 void add_f(const char* str) {
-    long long i = hash_f(str), j = 1;
+//    if (strcmp(str, "cabaab") == 0) {
+//        printf("hello");
+//    }
+    long long i = hash_f(str); //j = 1;
     while (1) {
         if (strcmp(hash_table[i], "EMPTY") == 0) {
+//        if (strcmp(hash_table[i], "EMPTY") == 0 || strcmp(hash_table[i], "_DELETE_") == 0) {
             strcpy(hash_table[i], str);
             return;
         }
         if (strcmp(hash_table[i], str) == 0)
             return;
-        i = (i + j) % HASH_SIZE;
-        ++j;
+        i = (i + 1) % HASH_SIZE;
+//        ++j;
+//        ++i;
     }
 }
 
@@ -67,7 +79,11 @@ void delete_f(const char* str) {
     long long i = search_f(str);
     if (i == -1)
         return;
-    strcpy(hash_table[i], "EMPTY");
+    strcpy(hash_table[i], "_DELETE_");
+
+    //rehashing table
+//    while (strcmp(hash_table[i+1], "EMPTY") != 0)
+//        strcpy(hash_table[i], hash_table[i+1]);
 }
 
 int main() {
@@ -82,16 +98,24 @@ int main() {
     make_hash_table();
 
     char c, word[15];
+    int i = 0;
     while (1) {
         fscanf(input, "%c", &c);
         if (c == '+') {
             fscanf(input, "%s", word);
+//            if (strcmp(word, "cbaaab") == 0) {
+//                ++i;
+//                if (i == 3)
+//                    printf("hello");
+//            }
             add_f(word);
         } else if (c == '-') {
             fscanf(input, "%s", word);
             delete_f(word);
         } else if (c == '?') {
             fscanf(input, "%s", word);
+//            if (strcmp(word, "cbaaab") == 0)
+//                printf("hello");
             long long s = search_f(word);
             if (s == -1)
                 fprintf(output, "-%s\n", word);
@@ -102,6 +126,8 @@ int main() {
             break;
     }
 
+
+//    printf("%d", 'd' - 'a');
     fclose(input);
     fclose(output);
 
