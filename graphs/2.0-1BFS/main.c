682cb4e8be0083b05f2f76d6d9e6d34f4c8514ed
Vagabun <16858610+Vagabun@users.noreply.github.com>
2017-11-25 22:12:05 +0200

    graphs: 2.0,1-BFS - add graph structure, read operation

--- graphs/2.0-1BFS/main.c
+++ graphs/2.0-1BFS/main.c
@@ -1,17 +1,30 @@
 #include <stdio.h>
-#define MAXSIZE 2000020
+#include <stdlib.h>
+#include <limits.h>
+#define DEQ_MAXSIZE 2000020
+#define MAXSIZE 100010
+#define ZERO 1
+#define ONE 2
 
-int deq[MAXSIZE];
+int deq[DEQ_MAXSIZE];
+int edges[MAXSIZE][MAXSIZE];
+//int vertices[MAXSIZE][MAXSIZE];
+int vertices[MAXSIZE];
+//int temporar[MAXSIZE][MAXSIZE];
+//int tem[100000][10000];
 
+int N, M;
+
+//deque type and operations
 typedef struct {
     int *data;
     int head, tail;
 } deque;
 
-void init(deque *inst) {
+void init_deque(deque *inst) {
     inst->data = deq;
-    inst->head = (MAXSIZE / 2) - 1;
-    inst->tail = (MAXSIZE / 2) - 1;
+    inst->head = (DEQ_MAXSIZE / 2) - 1;
+    inst->tail = (DEQ_MAXSIZE / 2) - 1;
 }
 
 int empty(deque *inst) {
@@ -40,11 +53,56 @@ int pop_front(deque *inst) {
     return inst->data[inst->head++];
 }
 
+//graph type and operations
+typedef struct {
+    int dist, parent;
+} vertex;
+
+typedef struct {
+    vertex data[MAXSIZE];
+} graph;
+
+void init_graph(graph *g) {
+    int i;
+//    g->data = (vertex*)vertices;
+    for (i = 1; i <= N; ++i) {
+        g->data[i].parent = 0;
+        g->data[i].dist = INT_MAX;
+    }
+}
+
+void read_data(FILE *input) {
+    int i, u, v, d;
+    fscanf(input, "%d %d", &N, &M);
+    for (i = 1; i <= M; ++i) {
+        fscanf(input, "%d %d %d", &u, &v, &d);
+        if (v > edges[u][0])
+            edges[u][0] = v;
+        edges[u][v] = d + 1;
+    }
+}
+
 int main() {
-    deque d;
-    init(&d);
-    push_back(&d, 20);
-    push_front(&d, 30);
+//    deque d;
+//    init_deque(&d);
+//    push_back(&d, 20);
+//    push_front(&d, 30);
+
+//    edges = malloc(sizeof(int*) * MAXSIZE);
+//    int i;
+//    for (i = 0; i < MAXSIZE; ++i)
+//        edges[i] = malloc(sizeof(int) * MAXSIZE);
+
+//    graph g;
+//    N = 10;
+//    init_graph(&g);
+//    edges[1][0] = 6;
+//    int i;
+//    for (i = 1; i <= MAXSIZE; ++i)
+//        edges[i] = -1;
+
+    FILE *input = fopen("input.txt", "r");
+    read_data(input);
 
     printf("test\n");
 
