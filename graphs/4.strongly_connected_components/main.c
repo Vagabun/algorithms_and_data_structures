77fd3e3088742be970e280717383dd427a84acfa
Vagabun <16858610+Vagabun@users.noreply.github.com>
2017-12-10 23:27:14 +0200

    graphs: 4.strongly connected components - fix segfault, sent for test

--- graphs/4.strongly_connected_components/main.c
+++ graphs/4.strongly_connected_components/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define MAXSIZE 100100
+#define MAXSIZE 100010
 
 int vertices[MAXSIZE] = {0};
 
@@ -21,17 +21,10 @@ typedef struct {
 } adj_list;
 
 void init_adj_list(adj_list *a_list) {
-    a_list->array = malloc(N * sizeof(adj_list_data));
+    a_list->array = malloc((N + 10) * sizeof(adj_list_data));
     int i;
     for (i = 1; i <= N; ++i)
         a_list->array[i].head = NULL;
-}
-
-void adj_list_insert(adj_list *a_list, int source, int destination) {
-    node_t *new_node = malloc(sizeof(node_t));
-    new_node->vrtx = destination;
-    new_node->next = a_list->array[source].head;
-    a_list->array[source].head = new_node;
 }
 
 void free_list(adj_list *l) {
@@ -45,6 +38,14 @@ void free_list(adj_list *l) {
             free(tmp);
         }
     }
+    free(l->array);
+}
+
+void adj_list_insert(adj_list *a_list, int source, int destination) {
+    node_t *new_node = malloc(sizeof(node_t));
+    new_node->vrtx = destination;
+    new_node->next = a_list->array[source].head;
+    a_list->array[source].head = new_node;
 }
 
 //graph type and operations
@@ -57,7 +58,7 @@ typedef struct {
 } graph;
 
 void init_graph(graph *g) {
-    g->data = malloc(N * sizeof(vertex));
+    g->data = malloc((N + 10) * sizeof(vertex));
     int i;
     for (i = 1; i <= N; ++i) {
         g->data[i].status = 0;
@@ -111,21 +112,6 @@ int dfs_forest_original(graph *g, adj_list *l) {
     return k;
 }
 
-void read_data(FILE *input, adj_list *l1, adj_list *l2) {
-    int i, j, k, v;
-    fscanf(input, "%d", &N);
-    init_adj_list(l1);
-    init_adj_list(l2);
-    for (i = 1; i <= N; ++i) {
-        fscanf(input, "%d", &k);
-        for (j = 1; j <= k; ++j) {
-            fscanf(input, "%d", &v);
-            adj_list_insert(l1, i, v);
-            adj_list_insert(l2, v, i);
-        }
-    }
-}
-
 int main() {
 
     FILE *input = fopen("input.txt", "r");
@@ -136,18 +122,35 @@ int main() {
     graph *g_inv = malloc(sizeof(graph));
     graph *g_orig = malloc(sizeof(graph));
 
-    read_data(input, original, inverted);
-//    printf("hello\n");
+    //read graph from file and make adjacency lists
+    int i, j, k, v;
+    fscanf(input, "%d", &N);
+    init_adj_list(original);
+    init_adj_list(inverted);
+    for (i = 1; i <= N; ++i) {
+        fscanf(input, "%d", &k);
+        for (j = 1; j <= k; ++j) {
+            fscanf(input, "%d", &v);
+            adj_list_insert(original, i, v);
+            adj_list_insert(inverted, v, i);
+        }
+    }
+
     dfs_forest_inverted(g_inv, inverted);
-//
     fprintf(output, "%d\n", dfs_forest_original(g_orig, original));
 
-//    free_list(&original);
-//    free_list(&inverted);
-//    free(g_inv);
-//    free(g_orig);
+    //free adjacency lists and graphs
+    free_list(original);
+    free(original);
+    free_list(inverted);
+    free(inverted);
+    free(g_orig->data);
+    free(g_orig);
+    free(g_inv->data);
+    free(g_inv);
 
     fclose(input);
     fclose(output);
+
     return 0;
 }
