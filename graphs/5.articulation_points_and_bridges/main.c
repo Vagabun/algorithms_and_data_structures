8a821f7cfcb74ab96bfa9d9c2db0656f37643f2d
Vagabun <16858610+Vagabun@users.noreply.github.com>
2017-12-15 15:13:02 +0200

    graphs: 5.articulation points and bridges - in progress

--- graphs/5.articulation_points_and_bridges/main.c
+++ graphs/5.articulation_points_and_bridges/main.c
@@ -1,16 +1,36 @@
 #include <stdio.h>
 
-int N;
 int adj_list[1000][1000];
+int N, tm;
 
 typedef struct {
-    int data[1000];
+    int status, parent, root, art_point, t1, t2;
+} vertex;
+
+typedef struct {
+    vertex data[1000];
 } graph;
 
 void init_adj_list() {
     int i;
     for (i = 1; i <= N; ++i)
         adj_list[i][0] = 0;
+}
+
+void init_graph(graph *g) {
+    int i;
+    for (i = 1; i <= N; ++i) {
+        g->data[i].status = 0;
+        g->data[i].parent = 0;
+        g->data[i].art_point = 0;
+    }
+}
+
+void dfs(graph *g, int u, int r) {
+    tm = tm + 1;
+    g->data[u].t1 = tm;
+    g->data[u].status = 1;
+    g->data[u].root = r;
 }
 
 void read_data(FILE *input) {
