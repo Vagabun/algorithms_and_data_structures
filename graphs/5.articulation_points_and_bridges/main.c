a964471fc31829ecbb7ee8662e9c1f8ac8947de0
Vagabun <16858610+Vagabun@users.noreply.github.com>
2017-12-15 22:10:14 +0200

    graphs: 5.articulation points and bridges - dfs for articulation points, bridges in progress

--- graphs/5.articulation_points_and_bridges/main.c
+++ graphs/5.articulation_points_and_bridges/main.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
+#define MIN(a, b) (((a) < (b)) ? (a) : (b))
+#define MAX(a, b) (((a) > (b)) ? (a) : (b))
+
 int adj_list[1000][1000];
 int N, tm;
 
 typedef struct {
-    int status, parent, root, art_point, t1, t2;
+    int status, parent, root, art_point, t1, low;
 } vertex;
 
 typedef struct {
@@ -31,15 +34,58 @@ void dfs(graph *g, int u, int r) {
     g->data[u].t1 = tm;
     g->data[u].status = 1;
     g->data[u].root = r;
+    g->data[u].low = g->data[u].t1;
+    int l_max = 0;
+    int pow = 0;
+    int i;
+    for (i = 1; i <= adj_list[u][0]; ++i) {
+        if (g->data[adj_list[u][i]].status == 0) {
+            pow = pow + 1;
+            g->data[adj_list[u][i]].parent = u;
+            dfs(g, adj_list[u][i], r);
+            g->data[u].low = MIN(g->data[u].low, g->data[adj_list[u][i]].low);
+            l_max = MAX(l_max, g->data[adj_list[u][i]].low);
+        }
+        else if (g->data[u].parent != adj_list[u][i])
+           g->data[u].low = MIN(g->data[u].low, g->data[adj_list[u][i]].t1);
+    }
+    if ((u == g->data[u].root && pow > 1) || (u != g->data[u].root && l_max >= g->data[u].t1)) {
+        g->data[u].art_point = 1;
+        printf("%d - art point\n", u);
+    }
+}
+
+void bridges(graph *g) {
+//    int i, j;
+//    for (i = 1; i <= N; ++i) {
+//        if (g->data[i].art_point == 1) {
+//            for (j = 1; j <= adj_list[i][0]; ++j) {
+//                if (g->data[adj_list[i][j]].low >= g->data[i].t1)
+//                    printf("%d %d - bridge\n", i, adj_list[i][j]);
+//                else if (g->data[i].low >= g->data[adj_list[i][j]].t1)
+//                    printf("%d %d - bridge\n", i, adj_list[i][j]);
+//            }
+//        }
+//    }
+}
+
+void articulation_points(graph *g) {
+    init_graph(g);
+    tm = 0;
+    int i;
+    for (i = 1; i <= N; ++i) {
+        if (g->data[i].status == 0)
+            dfs(g, i, i);
+    }
 }
 
 void read_data(FILE *input) {
     fscanf(input, "%d", &N);
-    int i, u, v;
-    for (i = 1; i <= N; ++i) {
-        fscanf(input, "%d %d", &u, &v);
-        adj_list[u][++adj_list[u][0]] = v;
-        adj_list[v][++adj_list[v][0]] = u;
+    init_adj_list();
+    int u, v;
+    while (fscanf(input, "%d %d", &u, &v) != EOF) {
+       adj_list[u][++adj_list[u][0]] = v;
+       adj_list[v][++adj_list[v][0]] = u;
     }
 }
 
@@ -48,8 +94,10 @@ int main() {
     FILE *input = fopen("input.txt", "r");
     FILE *output = fopen("output.txt", "w");
 
-    init_adj_list();
+    graph G;
     read_data(input);
+    articulation_points(&G);
+    bridges(&G);
     printf("test\n");
     return 0;
 }
