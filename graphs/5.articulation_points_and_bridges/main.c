9777bbebd956a95b5bba288f923e4f237a4ddda7
Vagabun <16858610+Vagabun@users.noreply.github.com>
2017-12-24 22:33:06 +0200

    graphs: 5.articulation points and bridges - rewrite bcc search (delete bridges), sent for test

--- graphs/5.articulation_points_and_bridges/main.c
+++ graphs/5.articulation_points_and_bridges/main.c
@@ -7,33 +7,8 @@
 int adj_list[MAXSIZE][MAXSIZE];
 int connected_components_flag[MAXSIZE] = { 0 };
 int artic_points[MAXSIZE] = { 0 };
-int bc_comp[MAXSIZE] = { 0 };
 int bridges_stor[MAXSIZE][MAXSIZE];
-int N, tm, artic_points_counter = 0, bridges_counter = 0, bcc_flag = 0;
-
-//stack data type for using in search of biconnected components
-typedef struct {
-    int data[MAXSIZE];
-    int top;
-} stack;
-
-int isEmpty(stack *s) {
-    if (s->top == -1) return 1;
-    else return 0;
-}
-
-void push(stack *s, int vrtx) {
-    ++s->top;
-    s->data[s->top] = vrtx;
-}
-
-void pop(stack *s) {
-    --s->top;
-}
-
-int peek(stack *s) {
-    return s->data[s->top];
-}
+int N, tm, artic_points_counter = 0;
 
 //graph and vertex data type
 typedef struct {
@@ -65,8 +40,8 @@ void init_bridges_stor() {
         bridges_stor[i][0] = 0;
 }
 
-//dfs, bridges, bcc, a points
-void dfs(graph *g, int u, int r) {
+//dfs for ap, bridges, bcc
+void dfs_ap(graph *g, int u, int r) {
     tm = tm + 1;
     g->data[u].t1 = tm;
     g->data[u].status = 1;
@@ -79,7 +54,7 @@ void dfs(graph *g, int u, int r) {
         if (g->data[adj_list[u][i]].status == 0) {
             pow = pow + 1;
             g->data[adj_list[u][i]].parent = u;
-            dfs(g, adj_list[u][i], r);
+            dfs_ap(g, adj_list[u][i], r);
             g->data[u].low = MIN(g->data[u].low, g->data[adj_list[u][i]].low);
             l_max = MAX(l_max, g->data[adj_list[u][i]].low);
         }
@@ -93,50 +68,23 @@ void dfs(graph *g, int u, int r) {
     }
 }
 
-void dfs_bcc(graph *g, stack *s, int u, int r) {
-    if (u == 1)
-        bcc_flag = 1; //if we have vertex 1 in stack
+void dfs_bcc(graph *g, int u, int r) {
     g->data[u].status = 1;
     g->data[u].root = r;
     int i;
     for (i = 1; i <= adj_list[u][0]; ++i) {
+        if (bridges_stor[u][adj_list[u][i]] || bridges_stor[adj_list[u][i]][u])
+            continue;
         if (g->data[adj_list[u][i]].status == 0) {
             g->data[adj_list[u][i]].parent = u;
-            push(s, adj_list[u][i]);
-            dfs_bcc(g, s, adj_list[u][i], r);
-        }
-        else if (u == r) {
-            return;
+            dfs_bcc(g, adj_list[u][i], r);
         }
     }
 }
 
-void bc_components(graph *g_a_points, graph *g_bcc, stack *s) {
-    if (artic_points_counter > 0) {
-        s->top = -1;
-        int i;
-        for (i = 1; i <= N; ++i) {
-            if (g_a_points->data[i].art_point) {
-                push(s, i);
-                dfs_bcc(g_bcc, s, i, i);
-                if (bcc_flag) {
-                    while (!isEmpty(s)) {
-                        bc_comp[peek(s)]++;
-                        pop(s);
-                    }
-                    return;
-                }
-                else
-                    s->top = -1;
-            }
-        }
-    }
-    else {
-        //then all graph is biconnected
-        int k;
-        for (k = 1; k <= N; ++k)
-            bc_comp[k]++;
-    }
+void bc_components_search(graph *g) {
+    init_graph(g);
+    dfs_bcc(g, 1, 1);
 }
 
 void bridges(graph *g) {
@@ -149,7 +97,6 @@ void bridges(graph *g) {
                     int min = MIN(i, adj_list[i][j]), max = MAX(i, adj_list[i][j]);
                     bridges_stor[min][max]++;
                     bridges_stor[min][0] = MAX(bridges_stor[min][0], max);
-                    bridges_counter++;
                 }
             }
         }
@@ -158,7 +105,6 @@ void bridges(graph *g) {
                 if (g->data[adj_list[i][1]].art_point == 0) {
                     bridges_stor[i][adj_list[i][1]]++;
                     bridges_stor[i][0] = MAX(bridges_stor[i][0], adj_list[i][1]);
-                    bridges_counter++;
                 }
                 connected_components_flag[i]++;
                 connected_components_flag[adj_list[i][1]]++;
@@ -173,7 +119,7 @@ void articulation_points(graph *g) {
     int i;
     for (i = 1; i <= N; ++i) {
         if (g->data[i].status == 0)
-            dfs(g, i, i);
+            dfs_ap(g, i, i);
     }
 }
 
@@ -192,12 +138,10 @@ int main() {
     FILE *input = fopen("input.txt", "r");
     FILE *output = fopen("output.txt", "w");
 
-    graph G_a_points, G_bcc;
-    stack S;
+    graph G;
     read_data(input);
-    articulation_points(&G_a_points);
-    bridges(&G_a_points);
-    bc_components(&G_a_points, &G_bcc, &S);
+    articulation_points(&G);
+    bridges(&G);
 
     int i;
     if (artic_points_counter != 0) {
@@ -208,21 +152,21 @@ int main() {
         fprintf(output, "\n");
     }
 
-    if (bridges_counter != 0) {
-        for (i = 1; i <=N; ++i) {
-            if (bridges_stor[i][0] != 0) {
-                int j;
-                for (j = 1; j <= bridges_stor[i][0]; ++j) {
-                    if (bridges_stor[i][j] == 0)
-                        continue;
-                    fprintf(output, "%d %d\n", i, j);
-                }
+    for (i = 1; i <=N; ++i) {
+        if (bridges_stor[i][0] != 0) {
+            int j;
+            for (j = 1; j <= bridges_stor[i][0]; ++j) {
+                if (bridges_stor[i][j] == 0)
+                    continue;
+                fprintf(output, "%d %d\n", i, j);
             }
         }
     }
 
+    bc_components_search(&G);
+
     for (i = 1; i <= N; ++i) {
-        if (bc_comp[i])
+        if (G.data[i].status != 0)
             fprintf(output, "%d ", i);
     }
 
