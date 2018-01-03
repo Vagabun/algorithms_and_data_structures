3a6ac7920a10978e6cbb156bb487406911998710
Vagabun <16858610+Vagabun@users.noreply.github.com>
2018-01-03 19:29:46 +0200

    graphs: 7.disjoint set union - rewrite for hierarchical realization of DSU

--- graphs/8.disjoint_set_union/main.c
+++ graphs/8.disjoint_set_union/main.c
@@ -3,26 +3,35 @@
 #define MAXSIZE 100010
 
 int N;
-int representatives[MAXSIZE], cc[MAXSIZE] = { 0 };
+int parent[MAXSIZE], descendants[MAXSIZE];
 
 //DSU
 void make() {
     int i;
-    for (i = 1; i <= N; ++i)
-        representatives[i] = i;
+    for (i = 1; i <= N; ++i) {
+        parent[i] = 0;
+        descendants[i] = 1;
+    }
 }
 
 int rep(int u) {
-    return representatives[u];
+    while (parent[u] != 0)
+        u = parent[u];
+    return u;
 }
 
 void uni(int u, int v) {
     int r_1 = rep(u);
     int r_2 = rep(v);
-    int i;
-    for (i = 1; i <= N; ++i) {
-        if (rep(i) == r_1)
-            representatives[i] = r_2;
+    if (r_1 == r_2)
+        return;
+    if (descendants[r_1] > descendants[r_2]) {
+        parent[r_2] = r_1;
+        descendants[r_1] += descendants[r_2];
+    }
+    else {
+        parent[r_1] = r_2;
+        descendants[r_2] += descendants[r_1];
     }
 }
 
@@ -37,18 +46,13 @@ int main() {
     while (fscanf(input, "%d %d", &u, &v) != EOF)
         uni(u, v);
 
-    int i;
+    int i, connected_c = 0;
     for (i = 1; i <= N; ++i) {
-        cc[representatives[i]]++;
+        if (parent[i] == 0)
+            connected_c++;
     }
 
-    int counter = 0;
-    for (i = 1; i <= N; ++i) {
-        if (cc[i] != 0)
-            counter++;
-    }
-
-    fprintf(output, "%d", counter);
+    fprintf(output, "%d", connected_c);
 
     fclose(input);
     fclose(output);
